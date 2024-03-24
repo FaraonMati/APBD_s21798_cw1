756520b776c559cc3a5ecafb47a36230ff0c00bd
s21798 <s21798@pjwstk.edu.pl>
2024-03-24 23:43:55 +0100

    AVERAGE BRANCH COMMIT

--- APBD_s21798_1_console _app/Program.cs
+++ APBD_s21798_1_console _app/Program.cs
@@ -3,5 +3,21 @@ Console.WriteLine("APBD_1");
 Console.WriteLine("s21798");
 Console.WriteLine("console app");
 
+class feature_average
+{
+    static void Main()
+    {
+        var arrOfIntegers = new int[] { 12, 17, 25, 30, 6, 17, 156 };
+        var sum = 0;
+        for (int i = 0; i < arrOfIntegers.Length; i++)
+        {
+            sum += arrOfIntegers[i];
+        }
+        var avg = (float)sum / arrOfIntegers.Length;
+        Console.WriteLine("feature_average= " + avg);
+
+
+    }
+}
 
 
